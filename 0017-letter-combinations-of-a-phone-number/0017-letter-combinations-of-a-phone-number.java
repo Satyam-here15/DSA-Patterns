62fc2de8f6267dc680f90ef8392234960c2c5e99
Satyam Upadhyay <satyam.work15@gmail.com>
2026-08-02 11:34:25 +0530

    Time: 3 ms (29.64%), Space: 49 MB (14.17%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class Solution {
+
+    ArrayList<String> result = new ArrayList<>();
+
+    public void solve(int idx, String digits, String temp,
+                      HashMap<Character, String> mp) {
+
+        if (idx >= digits.length()) {
+            result.add(temp);
+            return;
+        }
+
+        char ch = digits.charAt(idx);
+        String str = mp.get(ch);
+
+        for (int i = 0; i < str.length(); i++) {
+
+            // Do
+            temp += str.charAt(i);
+
+            solve(idx + 1, digits, temp, mp);
+
+            // Undo (Backtracking)
+            temp = temp.substring(0, temp.length() - 1);
+        }
+    }
+
+    public List<String> letterCombinations(String digits) {
+
+        if (digits.length() == 0)
+            return new ArrayList<>();
+
+        HashMap<Character, String> mp = new HashMap<>();
+
+        mp.put('2', "abc");
+        mp.put('3', "def");
+        mp.put('4', "ghi");
+        mp.put('5', "jkl");
+        mp.put('6', "mno");
+        mp.put('7', "pqrs");
+        mp.put('8', "tuv");
+        mp.put('9', "wxyz");
+
+        String temp = "";
+
+        solve(0, digits, temp, mp);
+
+        return result;
+    }
+}
