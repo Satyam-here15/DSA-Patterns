f3d1f3c17d024ecec162a337048e021de867da4e
Satyam Upadhyay <satyam.work15@gmail.com>
2026-08-09 12:20:20 +0530

    Time: 0 ms (100%), Space: 44.6 MB (58.03%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,26 +1,49 @@
+// //====================================== BRUTE FORCE=====================================
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         int num=1;
+//         int i=0;
+//         int n = arr.length;
+//         while(i<n && k>0){
+//             if(arr[i] == num){
+//                 i++;
+
+//             }
+//             else{
+//                     k--;
+//             }
+//             num++;
+
+//         }
+
+//         while(i==n && k>0){
+//             num++;
+//             k--;
+//         }
+
+//         return num-1;
+//     }
+// }
+
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int num=1;
-        int i=0;
+
         int n = arr.length;
-        while(i<n && k>0){
-            if(arr[i] == num){
-                i++;
-                // num++;
+        int l = 0, r = n - 1;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid] - (mid + 1) < k) { //A[mid]-(mid+1)   --> This gives number of missing number before m'th index
+                l = mid + 1;
+            } else {
+                r = mid - 1;
             }
-            else{
-                    k--;
-            }
-            num++;
 
         }
 
-        while(i==n && k>0){
-            num++;
-            k--;
-        }
-       
-        return num-1;
+        return l + k;
 
     }
+
 }
