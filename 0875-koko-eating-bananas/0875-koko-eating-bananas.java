e45aa0fe04a364e18524e65f66b36b472dcde05a
Satyam Upadhyay <satyam.work15@gmail.com>
2026-08-10 10:57:38 +0530

    Time: 7 ms (82.01%), Space: 47.8 MB (75.25%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,62 +1,32 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
 
-        // Minimum possible eating speed
-        int low = 1;
+        int low=1;
+        int high =0;
 
-        // Maximum possible eating speed
-        // Find the largest pile
-        int high = 0;
-
-        for (int x : piles) {
-            high = Math.max(high, x);
+        for(int x:piles){
+            high = Math.max(x,high);
         }
 
-        // Binary Search
-        while (low < high) {
+        while(low<high){
+            int mid=low+(high-low)/2;
 
-            // mid = Koko ki possible eating speed
-            int mid = low + (high - low) / 2;
+            long hours=0;
 
-            // Total hours required if Koko eats
-            // 'mid' bananas per hour
-            long hours = 0;
-
-            for (int x : piles) {
-
-                // Hours required for this particular pile
-                //
-                // (x + mid - 1) / mid
-                // = ceil(x / mid)
-                //
-                // Example:
-                // x = 7, mid = 3
-                // (7 + 3 - 1) / 3
-                // = 9 / 3
-                // = 3 hours
-                hours += (x + mid - 1) / mid;
+            for(int x:piles){
+                hours+=(x+mid-1)/mid;
             }
-
-            // Koko can finish within h hours
-            if (hours <= h) {
-
-                // mid is a valid speed.
-                // But we want the MINIMUM speed,
-                // so try a smaller speed.
-                high = mid;
+            if(hours<=h){
+                high=mid;
             }
-
-            // Koko cannot finish within h hours
-            else {
-
-                // mid is too slow.
-                // We need a bigger speed.
-                low = mid + 1;
+            else{
+                low=mid+1;
             }
         }
-
-        // low == high
-        // This is the minimum speed that works.
         return low;
+
+
+
+
     }
 }
