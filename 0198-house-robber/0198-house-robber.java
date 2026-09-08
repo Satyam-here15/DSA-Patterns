3360dae0d28e62cb71435d0972a469d3e2f76ea1
Satyam Upadhyay <satyam.work15@gmail.com>
2026-09-08 12:34:50 +0530

    Time: 0 ms (100%), Space: 42.7 MB (56.52%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -14,38 +14,50 @@
 // }
 
 
-// =============================== DP ===========================================================
+// =============================== DP (Recursion + Memoization)===========================================================
+
+// class Solution {
+
+//     public int amount(int[] nums, int i, int[] dp){
+//         int n = nums.length;
+//         if(i>=n)return 0;
+
+//         if(dp[i]!=-1)return dp[i];
+
+//         int take = nums[i] + amount(nums, i+2, dp);
+//         int skip = amount(nums,i+1,dp);
+
+//         return dp[i] = Math.max(take,skip);
+//     }
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] dp = new int[n];
+
+//         Arrays.fill(dp,-1);
+
+//         return amount(nums,0,dp);
+//     }
+// }
+// ==================== DP (Tabulation) =============================================================
 
 class Solution {
 
-    public int amount(int[] nums, int i, int[] dp){
-        int n = nums.length;
-        if(i>=n)return 0;
+ public int rob(int[] nums) {
+    int n = nums.length;
+    if(n==1) return nums[0];
 
-        if(dp[i]!=-1)return dp[i];
+    int[] dp = new int[n];
 
-        int take = nums[i] + amount(nums, i+2, dp);
-        int skip = amount(nums,i+1,dp);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0],nums[1]);
 
-        return dp[i] = Math.max(take,skip);
+    for(int i=2;i<n;i++){
+        dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
     }
-    public int rob(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
+    return dp[n-1];
 
-        Arrays.fill(dp,-1);
-
-        return amount(nums,0,dp);
-
-
-    }
-
+ }
 }
-
-
-
-
-
 
 
 
