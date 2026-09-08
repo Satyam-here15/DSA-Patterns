2578defe66e5bbc9af4a49d18a260771e07aa3c2
Satyam Upadhyay <satyam.work15@gmail.com>
2026-09-08 11:39:47 +0530

    Time: 0 ms (100%), Space: 42.6 MB (87.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,30 +1,54 @@
+// // ========================== Brute Force Soln ===============================================
+// class Solution {
+//     public int rob(int[] nums) {
+
+//        int prev1 = 0, prev2 = 0;
+//         for (int num : nums) {
+//             int curr = Math.max(prev1, num + prev2);
+//             prev2 = prev1;
+//             prev1 = curr;
+//         }
+//         return prev1;
+        
+//     }
+// }
+
+
+// =============================== DP ===========================================================
+
 class Solution {
-    public int rob(int[] nums) {
 
-        // int n = nums.length;
+    public int amount(int[] nums, int i, int[] dp){
+        int n = nums.length;
+        if(i>=n)return 0;
 
-        // int count =0;
+        if(dp[i]!=-1)return dp[i];
 
-        // for(int i=0;i<nums.length;i++){
-        //     if(n%2==0){
-        //         count++;
-        
-                    
-        //     }
-        //     else{
-        //         count++;
-        //     }
-            
-        // }
-        // return count;
+        int take = nums[i] + amount(nums, i+2, dp);
+        int skip = amount(nums,i+1,dp);
 
-       int prev1 = 0, prev2 = 0;
-        for (int num : nums) {
-            int curr = Math.max(prev1, num + prev2);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-        return prev1;
-        
+        return dp[i] = Math.max(take,skip);
     }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+
+        Arrays.fill(dp,-1);
+
+        return amount(nums,0,dp);
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
